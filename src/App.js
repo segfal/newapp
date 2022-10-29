8c7119a471d08fae0920b81720f59ec76f6f940c
segfaultboi@gmail.com <segfaultboi@gmail.com>
2022-10-29 17:28:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+//grab json data from directory
+import data from './data.json'
+import {useEffect, useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
 }
 
 export default App;
